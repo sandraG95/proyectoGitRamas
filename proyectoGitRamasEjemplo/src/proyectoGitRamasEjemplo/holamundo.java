cb0991f808bb1abde1aed031c6c0b745bd191493
sandragallego <sandragallego@192.168.1.136>
2022-12-07 12:21:21 +0100

    hola mundo

--- proyectoGitRamasEjemplo/src/proyectoGitRamasEjemplo/holamundo.java
+++ proyectoGitRamasEjemplo/src/proyectoGitRamasEjemplo/holamundo.java
@@ -3,7 +3,7 @@ package proyectoGitRamasEjemplo;
 public class holamundo {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		System.out.println("hola amor");
 
 	}
 
